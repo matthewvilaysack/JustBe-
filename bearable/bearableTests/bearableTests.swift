aeb8e80bbb4448b09306a858870aa8011bb6659a
Andrea Hurtado <75586588+andreaax@users.noreply.github.com>
2025-01-29 14:02:44 -0800

    feat: added Swift starter files

---
+++ bearable/bearableTests/bearableTests.swift
@@ -0,0 +1,17 @@
+//
+//  bearableTests.swift
+//  bearableTests
+//
+//  Created by Andrea Hurtado on 1/29/25.
+//
+
+import Testing
+@testable import bearable
+
+struct bearableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
