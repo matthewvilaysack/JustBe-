c482428f44273170c6548c2b8d5efd8f0dea2f78
Andrea Hurtado <75586588+andreaax@users.noreply.github.com>
2025-01-29 13:15:58 -0800

    feat: added starter Swift files

---
+++ bearable/bearable/bearableApp.swift
@@ -0,0 +1,17 @@
+//
+//  bearableApp.swift
+//  bearable
+//
+//  Created by Andrea Hurtado on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct bearableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
