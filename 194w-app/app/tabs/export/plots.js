8c06c7f0b7acb8c7b1744e80113e93b7407557ad
Felicity H <felicityh15@gmail.com>
2025-03-08 16:14:38 -0800

    updating themes

--- 194w-app/app/tabs/export/plots.js
+++ 194w-app/app/tabs/export/plots.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ScrollView, View, Text, StyleSheet } from "react-native";
 import { fetchCountData, getHighestPainRatingPerDay } from "../../utils/supabase-helpers";
 import { VictoryChart, VictoryLine, VictoryAxis, VictoryTheme, VictoryPie } from "victory-native";
+import Theme from "@/src/theme/theme";
 
 // Pain Chart Component
 const PainChart = ({ data }) => {
@@ -16,21 +17,36 @@ const PainChart = ({ data }) => {
     }));
     // console.log("formatted data: ", formattedData);
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Highest Pain Severity Over Time</Text>
-        <VictoryChart theme={VictoryTheme.material} scale={{ x: "time" }}>
-            <VictoryAxis 
-                tickFormat={(t) => 
-                    new Date(t).toLocaleDateString("en-US", { month: "short", day: "numeric" })
-                } 
-            />
-            <VictoryAxis dependentAxis />
-            <VictoryLine 
-                data={formattedData} 
-                style={{ data: { stroke: "#c43a31" } }} 
-            />
-        </VictoryChart>
-      </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+        <View style={styles.container}>
+            <Text style={styles.title}>Highest Pain Severity Over Time</Text>
+        
+            <VictoryChart theme={VictoryTheme.clean} scale={{ x: "time" }}
+            padding={{ left: 50, right: 50, top: 20, bottom: 50 }}>
+                <VictoryAxis 
+                    style={{
+                        axis: { stroke: Theme.colors.lightBlue }, // Changes the axis line color
+                        ticks: { stroke: Theme.colors.darkBlue}, // Changes tick color
+                        tickLabels: { fill: Theme.colors.darkBlue, fontWeight: "bold" }, 
+                    }}                
+                    tickFormat={(t) => 
+                        new Date(t).toLocaleDateString("en-US", { month: "short", day: "numeric" })
+                    } 
+                />
+                <VictoryAxis dependentAxis 
+                    style={{
+                        axis: { stroke: Theme.colors.lightBlue }, // Changes the axis line color
+                        ticks: { stroke: Theme.colors.darkBlue}, 
+                        tickLabels: { fill: Theme.colors.darkBlue, fontWeight: "bold" }, 
+                    }}/>
+                <VictoryLine 
+                    data={formattedData} 
+                    style={{ data: { stroke: Theme.colors.darkPurple } }} 
+                />
+            </VictoryChart> 
+        </View>
+    </ScrollView>
+       
   );
 };
 
@@ -39,6 +55,7 @@ const PieChart = ({ data, title }) => {
   if (!data || Object.keys(data).length === 0) {
       return; // <Text>Loading data...</Text>;
   }
+  const palette = [Theme.colors.darkPurple, "lightblue", Theme.colors.darkBlue, Theme.colors.purple, Theme.colors.testColor];
 
   // Convert JSON object into an array format that VictoryPie understands
   const formattedData = Object.entries(data).map(([key, value]) => ({
@@ -52,7 +69,8 @@ const PieChart = ({ data, title }) => {
         <Text style={styles.title}> Most Common {title} </Text>
         <VictoryPie 
             data={formattedData}
-            colorScale={["#4CAF50", "#FF9800", "#2196F3", "#E91E63", "#9C27B0"]}
+            // colorScale={["#4CAF50", "#FF9800", "#2196F3", "#E91E63", "#9C27B0"]}
+            colorScale={palette}
             labelRadius={50}
             labels={({ datum }) => `${datum.x}: ${datum.y}`} // Display both key and value
             style={{
@@ -96,15 +114,15 @@ const PlotDisplayer = () => {
     return (
       <ScrollView >
         <View style={styles.container}>
-            <PainChart data={pain_data} />
+            <PainChart data={pain_data}/>
 
             <PieChart data={count_data.duration} title="Durations"/>
 
             <PieChart data={count_data["when-does-it-hurt"]} title="Timings" />
+            
+            <PieChart data={count_data.causes} title="Causes"/>
 
             <PieChart data={count_data.concerns} title="Concerns"/>
-
-            <PieChart data={count_data.causes} title="Causes"/>
         </View>
       </ScrollView>
     );
@@ -117,12 +135,11 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: "center",
       justifyContent: "center",
-      padding: 20
+      padding: 10
   },
   title: {
       fontSize: 20,
-      fontWeight: "bold",
-      marginBottom: 10
+      fontWeight: "bold"
   }
 });
 
